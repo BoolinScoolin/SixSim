af879bf6e3e0a69d505080697a4c463c9effb0d8
BoolinScoolin <colinriba@ufl.edu>
2026-07-12 16:17:59 -0700

    consolidated rigid body derivative

--- sim/include/sixsim/sim/rigid_body_derivative.hpp
+++ sim/include/sixsim/sim/rigid_body_derivative.hpp
@@ -33,12 +33,51 @@ inline RigidBodyDerivative combine_derivatives(
   return derivative;
 }
 
-}  // namespace sixsim::sim
+inline RigidBodyDerivative kinematic_derivative(const RigidBodyState& state) {
+  RigidBodyDerivative derivative{};
+  derivative.position_ned_derivative_mps =
+      math::rotate(state.q_body2ned, state.velocity_body_mps);
+  derivative.q_body2ned_derivative =
+      math::derivative_from_body_rate(state.q_body2ned, state.omega_body_rps);
+  return derivative;
+}
 
-#include "sixsim/sim/rigid_body_dynamics.hpp"
-#include "sixsim/sim/rigid_body_kinematics.hpp"
+inline math::Vector3 compute_accel_body(const ForceMoment& force_moment,
+                                        const MassProperties& mass_properties) {
+  return force_moment.force_body_n / mass_properties.mass_kg;
+}
 
-namespace sixsim::sim {
+inline math::Vector3 compute_omega_dot_body(
+    const ForceMoment& force_moment,
+    const MassProperties& mass_properties,
+    const math::Vector3& omega_body_rps) {
+  const math::Vector3 angular_momentum_body{
+      mass_properties.inertia_body_kgm2.x * omega_body_rps.x,
+      mass_properties.inertia_body_kgm2.y * omega_body_rps.y,
+      mass_properties.inertia_body_kgm2.z * omega_body_rps.z,
+  };
+  const math::Vector3 net_moment_body =
+      force_moment.moment_body_nm -
+      math::cross(omega_body_rps, angular_momentum_body);
+
+  return {
+      net_moment_body.x / mass_properties.inertia_body_kgm2.x,
+      net_moment_body.y / mass_properties.inertia_body_kgm2.y,
+      net_moment_body.z / mass_properties.inertia_body_kgm2.z,
+  };
+}
+
+inline RigidBodyDerivative dynamic_derivative(
+    const RigidBodyState& state,
+    const ForceMoment& force_moment,
+    const MassProperties& mass_properties) {
+  RigidBodyDerivative derivative{};
+  derivative.velocity_body_derivative_mps2 =
+      compute_accel_body(force_moment, mass_properties);
+  derivative.omega_body_derivative_rps2 =
+      compute_omega_dot_body(force_moment, mass_properties, state.omega_body_rps);
+  return derivative;
+}
 
 inline RigidBodyDerivative rigid_body_derivative(
     const RigidBodyState& state,
