827aa8d885ceb3268c8a4a26e870d9dbb02ae064
BoolinScoolin <colinriba@ufl.edu>
2026-07-12 15:47:10 -0700

    added combine_force_moment function

--- sim/include/sixsim/sim/force_moment.hpp
+++ sim/include/sixsim/sim/force_moment.hpp
@@ -9,4 +9,19 @@ struct ForceMoment {
   math::Vector3 moment_body_nm{};
 };
 
+inline ForceMoment combine_force_moment(const ForceMoment& a,
+                                        const ForceMoment& b) {
+  ForceMoment combined{};
+  combined.force_body_n = a.force_body_n + b.force_body_n;
+  combined.moment_body_nm = a.moment_body_nm + b.moment_body_nm;
+  return combined;
+}
+
+inline ForceMoment combine_force_moment(const ForceMoment& gravity,
+                                        const ForceMoment& aerodynamics,
+                                        const ForceMoment& propulsion) {
+  return combine_force_moment(
+      combine_force_moment(gravity, aerodynamics), propulsion);
+}
+
 }  // namespace sixsim::sim
